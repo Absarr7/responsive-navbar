795f90c79d8733eb3015b887817a9eaed0f92745
Absar Ahmad <absar4060@gmail.com>
2022-05-18 15:58:23 +0530

    responsive navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+// Navbar Responsiveness.
+const hamburger = document.querySelector('.hamburger');
+const mobile_nav = document.querySelector('.mobile-nav')
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('is-active');
+    mobile_nav.classList.toggle('is-active');
+
+
+})
